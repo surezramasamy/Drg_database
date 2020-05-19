647a6491361728a2dca3bd73ff4e72ccae7cd32e
surezramasamy <surezramasamy@gmail.com>
2020-05-20 00:38:20 +0530

    modified

--- drg_proj/bom/admin.py
+++ drg_proj/bom/admin.py
@@ -1,24 +1,25 @@
 from django.contrib import admin
-from . models import Model_Name,Sub_Assembly,Item_Detail
+from . models import Model_Name,Sub_Assembly,Item_Detail,Material
 from import_export import fields,resources
 from import_export.fields import Field
 from import_export.admin import ImportExportModelAdmin
-
+from import_export.widgets import ForeignKeyWidget
 
 class BookResource(resources.ModelResource):
-    Model_Name = fields.Field(attribute = 'Model_Name',)
-    Sub_Assembly_Name = fields.Field(attribute = 'Sub_Assembly_Name',)
+    Model_Name = fields.Field(column_name='Model_Name',attribute = 'Model_Name',widget=ForeignKeyWidget(Model_Name, 'model'))
+    Sub_Assembly_Name = fields.Field(column_name='Sub_Assembly_Name',attribute = 'Sub_Assembly_Name',widget=ForeignKeyWidget(Sub_Assembly, 'sub_assembly'))
+    Raw_Material = fields.Field(column_name='Raw_Material',attribute = 'Raw_Material',widget=ForeignKeyWidget(Material, 'material'))
 
     class Meta:
         model = Item_Detail
-    export_order = ('Model_Name','Sub_Assembly_Name','Size','Drawing')
+
 
 
 class BookAdmin(ImportExportModelAdmin,admin.ModelAdmin):
     resource_class = BookResource
     class Meta:
         model = Item_Detail
-    list_display = ['Model_Name','Sub_Assembly_Name','Size','Drawing']
+    list_display = ['Model_Name','Sub_Assembly_Name','Size','Raw_Material','Drawing1','Drawing2','Photo1','Photo2','Process_Instructions']
     search_fields = ['Model_Name','Sub_Assembly_Name']
     list_filter = ['Model_Name','Sub_Assembly_Name']
 
@@ -27,3 +28,4 @@ class BookAdmin(ImportExportModelAdmin,admin.ModelAdmin):
 admin.site.register(Item_Detail,BookAdmin)
 admin.site.register(Model_Name)
 admin.site.register(Sub_Assembly)
+admin.site.register(Material)
--- drg_proj/bom/models.py
+++ drg_proj/bom/models.py
@@ -10,11 +10,20 @@ class Sub_Assembly(models.Model):
     sub_assembly =models.CharField(max_length=256)
     def __str__(self):
         return self.sub_assembly
+class Material(models.Model):
+    material =models.CharField(max_length=256)
+    def __str__(self):
+        return self.material
 
 class Item_Detail(models.Model):
-    Model_Name = models.ForeignKey(Model_Name,on_delete=models.CASCADE)
-    Sub_Assembly_Name = models.ForeignKey(Sub_Assembly,on_delete=models.CASCADE)
-    Size =models.CharField(max_length=256)
-    Drawing=models.FileField()
+    Model_Name = models.ForeignKey(Model_Name,on_delete=models.CASCADE,null=True)
+    Sub_Assembly_Name = models.ForeignKey(Sub_Assembly,on_delete=models.CASCADE,null=True)
+    Size = models.CharField(max_length=256,null=True)
+    Raw_Material = models.ForeignKey(Material,on_delete=models.CASCADE,null=True)
+    Drawing1 = models.FileField(blank=True,null=True)
+    Drawing2 = models.FileField(blank=True,null=True)
+    Photo1 = models.ImageField(help_text='optional',upload_to='images/',blank=True,null=True)
+    Photo2 = models.ImageField(help_text='optional',upload_to='images/',blank=True,null=True)
+    Process_Instructions = models.TextField(help_text='optional',blank=True,null=True)
     #def __str__(self):
         #return self.Size
